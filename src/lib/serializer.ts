56d806b151b5bcca6bc5cd5c176973a71a9506de
Jaroslav Šmolík <grissius@gmail.com>
2019-05-30 07:31:02 +0200

    Add toc

--- src/lib/serializer.ts
+++ src/lib/serializer.ts
@@ -9,15 +9,22 @@ ${sb.options.styles.join('')}
     <div id="title-page">
         <img src="https://i.imgur.com/oWj3Ecr.png" />
         <h1>${sb.title}</h1>
-        <h3>${sb.subtitle}</h3>
+        <h2>${sb.subtitle}</h2>
         <p>${sb.meta}</p>
+        <p>Creted with ️️<a href="http://zpevniky.com">zpevniky.com</a> and <a href="https://github.com/grissius/zcraper">grissius/zcraper</a></p>
+    </div>
+    <div id="toc">
+        <h2>Obsah</h2>
+        <ol>
+            ${sb.songs.map(s => `<li><a href="#${s.id}">${s.title}</a></li>`).join('')}
+        </ol>
     </div>
     <div id="content">
     ${sb.songs
         .map(
             s => `
         <div>
-            <h2>${s.title}</h2>
+            <h2 id="${s.id}">${s.title}</h2>
             ${s.chords}
         </div>
     `
